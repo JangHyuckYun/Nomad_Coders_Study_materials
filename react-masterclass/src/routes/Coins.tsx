ff0d2dc1c4759f78858971aad0cc70384c589904
by09019 <skg09203@naver.com>
2022-04-19 15:03:16 +0900

    delete react-master-class

--- react-masterclass/src/routes/Coins.tsx
+++
@@ -1,5 +0,0 @@
-function Coins() {
-  return <h1>asd</h1>;
-}
-
-export default Coins;
--- react-masterclass/src/theme.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultTheme } from 'styled-components';
-
-export const theme: DefaultTheme = {
-  bgColor: '#2f3640',
-  textColor: 'black',
-  btnColor: 'tomato',
-};
